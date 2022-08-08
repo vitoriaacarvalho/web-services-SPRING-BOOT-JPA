f8451631b6c89bd4a85a639c596a82fb0d86229f
vitoria <vitoriabccorrea@gmail.com>
2022-08-08 18:08:09 -0300

    Initializing Order department

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,11 +1,17 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity(name = "users") 
 @Table(name = "tb_user")
@@ -20,7 +26,11 @@ public class User implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
-
+	
+	@JsonIgnore
+	@OneToMany(mappedBy="client")
+	private List<Order> orders=new ArrayList<>(); 
+	
 	public User() {
 	}
 
@@ -96,6 +106,10 @@ public class User implements Serializable {
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	public List<Order> getOrders() {
+		return orders;
 	}
 }
 
